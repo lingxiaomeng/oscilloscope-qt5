86171c490b5761ace9145f911628ac34fd4c3db9
mlx <574243996@qq.com>
2019-12-22 21:28:13 +0800

    init

--- chartview.py
+++ chartview.py
@@ -17,12 +17,9 @@ class ChartView1(QChartView):
         self.layout = QHBoxLayout()
         self.setLayout(self.layout)
         self.callout = QLabel("")
-
         self.callout.setFont(QFont("Microsoft YaHei", 10, QFont.Bold, True))
         self.callout.setGeometry(0, 0, 0, 0)
-
         self.layout.addChildWidget(self.callout)
-
 
     def mouseMoveEvent(self, event: QMouseEvent):
         if self.is_clicking:
@@ -42,15 +39,17 @@ class ChartView1(QChartView):
 
             y_max = (self.chart().axisY().max())
             y_min = (self.chart().axisY().min())
-            print(y)
-            index = int((x - 90) / (765 / (x_max - x_min)) + x_min)
+            # print(y)
+            index = int((x - 90) / (465 / (x_max - x_min)) + x_min)
             y = (y - 35) / (200 / (y_max - y_min))
             y = y_max - y
             if index < len(self.ui.original_data_1):
                 y1 = self.ui.original_data_1[index].y()
+                y2 = self.ui.original_data_2[index].y()
                 if abs(y1 - y) < 2 and 85 < x < 856:
                     self.callout.setText("    x:%d, y:%.4f" % (int(index), y1))
                     self.callout.setGeometry(event.x(), event.y(), 220, 20)
+                    self.ui.xy_label.setText("Magnitude:%.8f  Phase:%.8f" % (y1, y2))
                 else:
                     self.callout.setText("")
 
@@ -73,72 +72,72 @@ class ChartView1(QChartView):
             self.chart().zoom(1.1)
 
 
-class ChartView2(QChartView):
-    def __init__(self, *__args, ui):
-        super().__init__(*__args)
-        self.ui = ui
-        self.is_clicking = False
-        self.x_old, self.y_old = 0, 0
-        self.layout = QHBoxLayout()
-        self.setLayout(self.layout)
-        self.xy_label = QLabel("")
-        self.callout = QLabel("")
-
-        self.callout.setFont(QFont("Microsoft YaHei", 10, QFont.Bold, True))
-        self.callout.setGeometry(0, 0, 0, 0)
-        self.xy_label.setFont(QFont("Microsoft YaHei", 10, QFont.Bold, True))
-        self.xy_label.setGeometry(330, 310, 300, 20)
-
-        self.layout.addChildWidget(self.callout)
-        self.layout.addChildWidget(self.xy_label)
-
-    def mouseMoveEvent(self, event: QMouseEvent):
-        if self.is_clicking:
-            if self.x_old == 0 and self.y_old == 0:
-                pass
-            else:
-                x = event.x() - self.x_old
-                y = event.y() - self.y_old
-                self.chart().scroll(-x, y)
-            self.x_old = event.x()
-            self.y_old = event.y()
-        else:
-            x = event.x()
-            y = event.y()
-            x_max = (self.chart().axisX().max())
-            x_min = (self.chart().axisX().min())
-
-            y_max = (self.chart().axisY().max())
-            y_min = (self.chart().axisY().min())
-            print(y)
-            index = int((x - 90) / (765 / (x_max - x_min)) + x_min)
-            y = (y - 35) / (200 / (y_max - y_min))
-            y = y_max - y
-            if index < len(self.ui.original_data_1):
-                y1 = self.ui.original_data_2[index].y()
-                y2 = self.ui.original_data_2[index].y()
-                if abs(y2 - y) < 2 and 85 < x < 856:
-                    self.callout.setText("    x:%d, y:%.4f" % (int(index), y2))
-                    self.xy_label.setText("x:%d, Data1:%.8f, Data2:%.8f" % (int(index), y1, y2))
-                    self.callout.setGeometry(event.x(), event.y(), 220, 20)
-                else:
-                    self.callout.setText("")
-                    self.xy_label.setText("")
-
-    def mousePressEvent(self, event: QMouseEvent):
-        if event.button() and event.button() == Qt.LeftButton:
-            self.is_clicking = True
-        elif event.button() and event.button() == Qt.RightButton:
-            self.chart().zoomReset()
-
-    def mouseReleaseEvent(self, event: QMouseEvent):
-        if self.is_clicking:
-            self.x_old, self.y_old = 0, 0
-            self.is_clicking = False
-        self.callout.setText("")
-
-    def wheelEvent(self, event: QWheelEvent):
-        if event.angleDelta().y() < 0:
-            self.chart().zoom(0.8)
-        else:
-            self.chart().zoom(1.1)
+# class ChartView2(QChartView):
+#     def __init__(self, *__args, ui):
+#         super().__init__(*__args)
+#         self.ui = ui
+#         self.is_clicking = False
+#         self.x_old, self.y_old = 0, 0
+#         self.layout = QHBoxLayout()
+#         self.setLayout(self.layout)
+#         self.xy_label = QLabel("")
+#         self.callout = QLabel("")
+#
+#         self.callout.setFont(QFont("Microsoft YaHei", 10, QFont.Bold, True))
+#         self.callout.setGeometry(0, 0, 0, 0)
+#         self.xy_label.setFont(QFont("Microsoft YaHei", 10, QFont.Bold, True))
+#         self.xy_label.setGeometry(100, 320, 400, 20)
+#
+#         self.layout.addChildWidget(self.callout)
+#         self.layout.addChildWidget(self.xy_label)
+#
+#     def mouseMoveEvent(self, event: QMouseEvent):
+#         if self.is_clicking:
+#             if self.x_old == 0 and self.y_old == 0:
+#                 pass
+#             else:
+#                 x = event.x() - self.x_old
+#                 y = event.y() - self.y_old
+#                 self.chart().scroll(-x, y)
+#             self.x_old = event.x()
+#             self.y_old = event.y()
+#         else:
+#             x = event.x()
+#             y = event.y()
+#             x_max = (self.chart().axisX().max())
+#             x_min = (self.chart().axisX().min())
+#
+#             y_max = (self.chart().axisY().max())
+#             y_min = (self.chart().axisY().min())
+#             # print(y)
+#             index = int((x - 90) / (465 / (x_max - x_min)) + x_min)
+#             y = (y - 35) / (200 / (y_max - y_min))
+#             y = y_max - y
+#             if index < len(self.ui.original_data_1):
+#                 y1 = self.ui.original_data_2[index].y()
+#                 y2 = self.ui.original_data_2[index].y()
+#                 if abs(y2 - y) < 20 and 85 < x < 856:
+#                     self.callout.setText("    x:%d, y:%.4f" % (int(index), y2))
+#                     self.xy_label.setText("Magnitude:%.8f  Phase:%.8f" % (y1, y2))
+#                     self.callout.setGeometry(event.x(), event.y(), 220, 20)
+#                 else:
+#                     self.callout.setText("")
+#                     self.xy_label.setText("")
+#
+#     def mousePressEvent(self, event: QMouseEvent):
+#         if event.button() and event.button() == Qt.LeftButton:
+#             self.is_clicking = True
+#         elif event.button() and event.button() == Qt.RightButton:
+#             self.chart().zoomReset()
+#
+#     def mouseReleaseEvent(self, event: QMouseEvent):
+#         if self.is_clicking:
+#             self.x_old, self.y_old = 0, 0
+#             self.is_clicking = False
+#         self.callout.setText("")
+#
+#     def wheelEvent(self, event: QWheelEvent):
+#         if event.angleDelta().y() < 0:
+#             self.chart().zoom(0.8)
+#         else:
+#             self.chart().zoom(1.1)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import math
+import random
 import sys
 from math import sin, cos
 
@@ -8,21 +9,25 @@ from PyQt5.Qt import *
 from PyQt5.QtChart import *
 from PyQt5.QtCore import *
 
-from chartview import ChartView1, ChartView2
+from chartview import ChartView1
 
 
 class MainUi(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
+
+        # self.statusBar().showMessage("Ready")
         self.start_range = 0
         self.end_range = 100
         self.count = 0
         self.original_data_1 = list()
         self.original_data_2 = list()
-        self.is_stop = False
+        self.original_data_3 = list()
 
-        self.setFixedSize(1200, 720)
+        self.is_stop = True
+
+        self.setFixedSize(1000, 740)
         self.main_widget = QtWidgets.QWidget()  # 创建窗口主部件
         self.main_layout = QtWidgets.QGridLayout()  # 创建主部件的网格布局
         self.main_widget.setLayout(self.main_layout)  # 设置窗口主部件布局为网格布局
@@ -30,14 +35,18 @@ class MainUi(QtWidgets.QMainWindow):
         # self.setWindowOpacity(0.9)  # 设置窗口透明度
         # self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 设置窗口背景透明
         # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
-        self.main_layout.setSpacing(5)  # 去除缝隙
+        self.main_layout.setSpacing(0)  # 去除缝隙
         self.chart1 = QChart()
         self.chart2 = QChart()
         self.chart_view1 = ChartView1(ui=self)
-        self.chart_view2 = ChartView2(ui=self)
+        self.chart_view2 = ChartView1(ui=self)
+        self.chart_view1.setObjectName("chart1")
+        self.chart_view2.setObjectName("chart2")
 
         self.series_1 = QLineSeries()
         self.series_2 = QLineSeries()
+        self.series_3 = QScatterSeries()
+        self.series_3.setName("series")
         self.stop_button = QPushButton("STOP")
         self.timer = QTimer()
         self.start_range_input = QLineEdit()
@@ -47,31 +56,57 @@ class MainUi(QtWidgets.QMainWindow):
         self.rang_btn = QPushButton("Enter")
         self.check_data1 = QCheckBox("Data 1")
         self.check_data2 = QCheckBox("Data 2")
-        self.save_btn = QPushButton("Save")
+        self.xy_label = QLabel("")
+        self.xy_label.setFont(QFont("Microsoft YaHei", 10, QFont.Bold, True))
 
-        self.main_layout.addWidget(self.stop_button, 5, 1, 1, 1)
-        self.main_layout.addWidget(self.save_btn, 5, 2, 1, 1)
-        self.main_layout.addWidget(self.start_range_input, 2, 1, 1, 1)
-        self.main_layout.addWidget(self.end_range_input, 2, 2, 1, 1)
-        self.main_layout.addWidget(self.label_1, 1, 1, 1, 1)
-        self.main_layout.addWidget(self.label_2, 1, 2, 1, 1)
-        self.main_layout.addWidget(self.rang_btn, 2, 3, 1, 1)
-        self.main_layout.addWidget(self.check_data1, 3, 1, 1, 1)
-        self.main_layout.addWidget(self.check_data2, 4, 1, 1, 1)
+        self.main_layout.addChildWidget(self.xy_label)
+        self.xy_label.setGeometry(615, 365, 350, 20)
+        # self.save_btn = QPushButton("Save")
+
+        # self.main_layout.addWidget(self.stop_button, 18, 40, 1, 1)
+        # self.main_layout.addWidget(self.save_btn, 5, 2, 1, 1)
+        # self.main_layout.addWidget(self.start_range_input, 2, 40, 1, 1)
+        # self.main_layout.addWidget(self.end_range_input, 2, 41, 1, 1)
+        # self.main_layout.addWidget(self.label_1, 1, 40, 1, 1)
+        # self.main_layout.addWidget(self.label_2, 1, 41, 1, 1)
+        # self.main_layout.addWidget(self.rang_btn, 2, 42, 1, 1)
+        # self.main_layout.addWidget(self.check_data1, 3, 40, 1, 1)
+        # self.main_layout.addWidget(self.check_data2, 4, 40, 1, 1)
 
         self.init_chart()
+        self.init_menu()
+        self.init_constellation_diagram()
         self.timer.timeout.connect(self.timer_slot)
         self.timer.start(20)
-        self.chart_view1.setFixedSize(900, 350)
-        self.chart_view2.setFixedSize(900, 350)
+        self.chart_view1.setFixedSize(600, 350)
+        self.chart_view2.setFixedSize(600, 350)
 
         self.check_data1.setChecked(True)
         self.check_data2.setChecked(True)
         self.stop_button.clicked.connect(self.stop_slot)
+        self.stop_button.setObjectName("stop_btu")
         self.rang_btn.clicked.connect(self.change_range)
         self.check_data1.stateChanged.connect(self.change_data)
         self.check_data2.stateChanged.connect(self.change_data)
-        self.save_btn.clicked.connect(self.save_data)
+        # self.save_btn.clicked.connect(self.save_data)
+        file = open('stylesheet.qss')
+        self.stylesheet = file.read()
+        # print(self.stylesheet)
+        self.setStyleSheet(self.stylesheet)
+
+    def init_menu(self):
+        file_menu = self.menuBar().addMenu('File')
+        save_action = QAction("Save", self)
+        load_action = QAction("Load", self)
+        save_action.triggered.connect(self.save_data)
+        file_menu.addAction(save_action)
+        file_menu.addAction(load_action)
+        self.start_action = QAction("Start", self)
+        option_menu = self.menuBar().addMenu('Options')
+        range_action = QAction("Range", self)
+        option_menu.addAction(range_action)
+        self.menuBar().addAction(self.start_action)
+        self.start_action.triggered.connect(self.stop_slot)
 
     def init_chart(self):
         self.chart1.addSeries(self.series_1)
@@ -80,7 +115,7 @@ class MainUi(QtWidgets.QMainWindow):
         self.series_2.setName("Data 2")
         self.chart1.createDefaultAxes()
         self.chart1.axisX().setRange(self.start_range, self.end_range)
-        self.chart1.axisY().setRange(-15, 15)
+        self.chart1.axisY().setRange(0, 15)
         self.chart1.legend().hide()
         self.chart1.axisX().setTitleFont(QFont("Microsoft YaHei", 10, QFont.Normal, True))
         self.chart1.axisY().setTitleFont(QFont("Microsoft YaHei", 10, QFont.Normal, True))
@@ -91,7 +126,7 @@ class MainUi(QtWidgets.QMainWindow):
 
         self.chart2.createDefaultAxes()
         self.chart2.axisX().setRange(self.start_range, self.end_range)
-        self.chart2.axisY().setRange(-15, 15)
+        self.chart2.axisY().setRange(0, 360)
         self.chart2.legend().hide()
         self.chart2.axisX().setTitleFont(QFont("Microsoft YaHei", 10, QFont.Normal, True))
         self.chart2.axisY().setTitleFont(QFont("Microsoft YaHei", 10, QFont.Normal, True))
@@ -106,12 +141,38 @@ class MainUi(QtWidgets.QMainWindow):
         self.chart_view2.setRenderHint(QPainter.Antialiasing)
         self.chart_view2.setChart(self.chart2)
 
-        self.main_layout.addWidget(self.chart_view1, 0, 0, 17, 1)
-        self.main_layout.addWidget(self.chart_view2, 17, 0, 17, 1)
-        self.chart1.legend().setVisible(True)
+        self.main_layout.addChildWidget(self.chart_view1)
+        self.main_layout.addChildWidget(self.chart_view2)
+        self.chart_view1.setGeometry(10, 10, 600, 350)
+        self.chart_view2.setGeometry(10, 365, 600, 350)
+
+        # self.chart1.legend().setVisible(True)
         self.chart1.legend().setAlignment(Qt.AlignBottom)
-        self.chart2.legend().setVisible(True)
+        # self.chart2.legend().setVisible(True)
         self.chart2.legend().setAlignment(Qt.AlignBottom)
+
+    def init_constellation_diagram(self):
+        self.constellation_chart = QPolarChart()
+        self.constellation_chart_view = QChartView()
+        self.constellation_chart_view.setObjectName("chart3")
+        self.constellation_chart_view.setChart(self.constellation_chart)
+        self.constellation_chart_view.setFixedSize(350, 350)
+        self.main_layout.addChildWidget(self.constellation_chart_view)
+        self.constellation_chart_view.setGeometry(615, 10, 350, 350)
+        self.radialAxis = QValueAxis()
+        self.radialAxis.setRange(0, 10)
+
+        self.angularAxis = QValueAxis()
+        self.angularAxis.setTickCount(9)
+        self.angularAxis.setRange(0, 360)
+        self.constellation_chart.legend().setVisible(False)
+        self.series_3.setMarkerSize(6.0)
+        self.constellation_chart.addSeries(self.series_3)
+        self.constellation_chart.addAxis(self.angularAxis, QPolarChart.PolarOrientationAngular)
+        self.constellation_chart.addAxis(self.radialAxis, QPolarChart.PolarOrientationRadial)
+        self.series_3.attachAxis(self.radialAxis)
+        self.series_3.attachAxis(self.angularAxis)
+        self.constellation_chart_view.setRenderHint(QPainter.Antialiasing)
 
     def save_data(self):
         filename = QFileDialog.getSaveFileName(self, 'save file', "", "Text Files (*.txt)")
@@ -156,46 +217,76 @@ class MainUi(QtWidgets.QMainWindow):
             print("value Error")
 
     def timer_slot(self):
-        self.update_data()
+        mag = 3 * sin(math.pi * self.count / 45) + 5
+        mag = random.random() * 3 + 5
+        rand = random.random()
+        phase = random.random() * 360
+        if not self.is_stop:
+            self.xy_label.setText("Magnitude:%.8f  Phase:%.8f" % (mag, phase))
+
+        # if rand < 0.25:
+        #     phase = 90
+        # elif rand < 0.5:
+        #     phase = 180
+        # elif rand < 0.75:
+        #     phase = 270
+        # else:
+        #     pass
+
+        self.update_data(mag, phase)
 
     def stop_slot(self):
         if self.is_stop:
+            self.start_action.setText('Stop')
+
             self.end_range = self.count
             self.start_range = self.end_range - 100
             self.is_stop = False
             data1 = self.original_data_1[self.start_range:self.end_range]
             data2 = self.original_data_2[self.start_range:self.end_range]
+            data3 = self.original_data_3[self.start_range:self.end_range]
 
             self.series_1.replace(data1)
             self.series_2.replace(data2)
+            self.series_3.replace(data3)
+
         else:
+            self.start_action.setText('Start')
             self.is_stop = True
 
-    def update_data(self):
+    def update_data(self, mag, phase):
         old_data_1 = self.series_1.pointsVector()
         old_data_2 = self.series_2.pointsVector()
+        old_data_3 = self.series_3.pointsVector()
         data_length = len(old_data_1)
         data_1 = old_data_1
         data_2 = old_data_2
+        data_3 = old_data_3
         if not self.is_stop and data_length > 200:
             data_1 = data_1[-200:-1]
             data_2 = data_2[-200:-1]
 
         for i in range(1):
-            point_1 = QPointF(self.count, 5 * sin(math.pi * self.count / 45))
-            point_2 = QPointF(self.count, 5 * cos(math.pi * self.count / 45))
+            point_1 = QPointF(self.count, mag)
+            point_2 = QPointF(self.count, phase)
+            point_3 = QPointF(phase, mag)
             data_1.append(point_1)
             data_2.append(point_2)
+            data_3.append(point_3)
             self.original_data_1.append(point_1)
             self.original_data_2.append(point_2)
-        self.series_1.replace(data_1)
-        self.series_2.replace(data_2)
+            self.original_data_3.append(point_3)
+        if not self.is_stop:
+            self.series_3.replace(data_3)
+            self.series_1.replace(data_1)
+            self.series_2.replace(data_2)
         self.count += 1
         if data_length > 100 and not self.is_stop:
             self.start_range += 1
             self.end_range += 1
             self.chart1.axisX().setRange(self.start_range, self.end_range)
             self.chart2.axisX().setRange(self.start_range, self.end_range)
+            # self.series_3.replace(data_3)
 
             # self.chart.scroll(2, 0)
 
